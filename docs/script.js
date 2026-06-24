a306648c7301e8dcddc4e9b99b4c0eb84e440d45
Reality Prince <sperma.cd@gmail.com>
2026-06-24 07:38:46 +0000

    regrouped

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
-import { Memory } from "./core/memory.js";
-import { parse } from "./core/parser.js";
-import { runNERODIS } from "./core/engine.js";
+import { Memory } from "../core/memory.js";
+import { parse } from "../core/parser.js";
+import { runNERODIS } from "../core/engine.js";
 
 window.runNERODIS = function () {
   const input = document.getElementById("input").value;
